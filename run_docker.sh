3c63e50c8fe55694da3a43a3fb41a0be8f1d34a8
Martino Bettucci <martinobettucci@users.noreply.github.com>
2025-06-18 12:54:50 +0200

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -4,4 +4,4 @@ set -e
 IMAGE_NAME="hackathon-chat-app"
 
 docker build -t "$IMAGE_NAME" .
-docker run -d --restart always -p 127.0.0.1:8088:8088 "$IMAGE_NAME"
+docker run -d --restart always -p 127.0.0.1:8086:8086 "$IMAGE_NAME"
